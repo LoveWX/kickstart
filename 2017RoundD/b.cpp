484d0f4706a9d85c90afeb193addf85326aafb62
LoveWX <ztf0025@gmail.com>
2020-12-30 01:14:51 +0800

    Update b.cpp

--- 2017RoundD/b.cpp
+++ 2017RoundD/b.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <cstring>
 #include <map>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
-const int SIZE = 100;
+const int SIZE = 100000;
 int x[SIZE], y[SIZE];
 int r[SIZE], s[SIZE];
 int A[SIZE], B[SIZE];
@@ -13,9 +14,9 @@ int apresum[SIZE], bpresum[SIZE];
 int amax[SIZE], amin[SIZE];
 int bmax[SIZE], bmin[SIZE];
 int n; // bit used
-int bit[SIZE + SIZE + 1];
+long long bit[SIZE + 3];
 
-void update(int idx, int diff)
+void update(int idx, long long diff)
 {
     while (idx <= n)
     {
@@ -24,9 +25,9 @@ void update(int idx, int diff)
     }
 }
 
-int prefixSum(int idx)
+long long prefixSum(int idx)
 {
-    int sum = 0;
+    long long sum = 0;
     while (idx != 0)
     {
         sum += bit[idx];
@@ -35,44 +36,45 @@ int prefixSum(int idx)
     return sum;
 }
 
-int rangeSum(int idx1, int idx2)
+long long rangeSum(int idx1, int idx2)
 {
     return prefixSum(idx2) - prefixSum(idx1 - 1);
 }
 
-int CalcGreater(int *P, int X)
+long long CountGreater(map<int,int> &m, int *P, int X)
 {
-    map<int, int> m;
-    for (int i = 0; i < N; ++i)
-    {
-        m[P[i]];
-    }
-    for (int i = 0; i < N; ++i)
-    {
-        m[P[i] + X];
-    }
-    n = 1;
-    for (auto it = m.begin(); it != m.end(); ++it)
-    {
-        it->second = n++;
-    }
-    memset(bit, 0, (n + 1) * sizeof(int));
-    int ans = 0;
+    memset(bit, 0, (n + 1) * sizeof(long long));
+    long long ans = 0;
     for (int i = N - 1; i >= 0; --i)
     {
+        ans += rangeSum(m.upper_bound(P[i] + X)->second, n);
         update(m[P[i]], 1);
-        ans += rangeSum(m[P[i] + X] + 1, n);
+        if (P[i] > X)
+        {
+            ++ans;
+        }
     }
     return ans;
 }
 
 void CalcKMaxSubarraySum(int *P, int *vmax)
 {
+    map<int, int> m1;
+    for (int i = 0; i < N; ++i)
+    {
+        m1[P[i]];
+    }
+    m1[0x7FFFFFFF];
+    n = 1;
+    for (auto it = m1.begin(); it != m1.end(); ++it)
+    {
+        it->second = n++;
+    }
     int left = -100000000, right = 100000001, mid;
     while (left < right)
     {
         mid = (right - left) / 2 + left;
-        if (CalcGreater(apresum, mid) > K - 1)
+        if (CountGreater(m1, P, mid) >= K)
         {
             left = mid + 1;
         }
@@ -82,23 +84,24 @@ void CalcKMaxSubarraySum(int *P, int *vmax)
         }
     }
     int count = 0;
-    for (int i = 0; i < n; ++i)
+    for (int i = 0; i<N; ++i)
     {
         if (P[i] > left)
         {
             vmax[count++] = P[i];
         }
-        for (int j = N - 1; j >= 0; --j)
+    }
+    map<int, int> m;
+    for (int i = N - 1; i >= 0 && count < K; --i)
+    {
+        for (auto it = m.upper_bound(P[i] + left); it != m.end(); ++it)
         {
-            if (P[j] - P[i] > left)
+            for (int j = it->second; j > 0; --j)
             {
-                vmax[count++] = P[j] - P[i];
-            }
-            else
-            {
-                break;
+                vmax[count++] = it->first - P[i];
             }
         }
+        m[P[i]] += 1;
     }
     for (; count < K; ++count)
     {
@@ -106,24 +109,52 @@ void CalcKMaxSubarraySum(int *P, int *vmax)
     }
 }
 
-long long CalcGreaterProduct(int X)
+long long CountGreaterProduct(int *a, int *b, int len, long long X)
 {
-    int ans = 0;
-    for (int i = 0; i < K; ++i)
+    long long ans = 0;
+    for (int i = 0; i < len; ++i)
     {
-
+        long long ai = a[i];
+        if (ai >= 0)
+        {
+            int left = 0, right = len, mid;
+            while (left < right)
+            {
+                mid = (right - left) / 2 + left;
+                if (ai * b[mid] > X)
+                {
+                    right = mid;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            ans += len - left;
+        }
+        else
+        {
+            int left = 0, right = len, mid;
+            while (left < right)
+            {
+                mid = (right - left) / 2 + left;
+                if (ai * b[mid] > X)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid;
+                }
+            }
+            ans += left;
+        }
     }
+    return ans;
 }
 
-void Calc()
+long long CalcLarge()
 {
-    apresum[0] = A[0];
-    bpresum[0] = B[0];
-    for (int i = 1; i < N; ++i)
-    {
-        apresum[i] = apresum[i - 1] + A[i];
-        bpresum[i] = bpresum[i - 1] + B[i];
-    }
     CalcKMaxSubarraySum(apresum, amax);
     CalcKMaxSubarraySum(bpresum, bmax);
     sort(amax, amax + K);
@@ -142,20 +173,74 @@ void Calc()
     }
     sort(amin, amin + K);
     sort(bmin, bmin + K);
-    for(int i)
+    long long left = -10000000000000000LL, right = 10000000000000001LL, mid;
+    while (left < right)
+    {
+        mid = (right - left) / 2 + left;
+        if (CountGreaterProduct(amax, bmax, K, mid) + CountGreaterProduct(amin, bmin, K, mid) >= K)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid;
+        }
+    }
+    return left;
+}
+
+long long CalcSmall()
+{
+    int len = N * (N + 1) / 2;
+    int count = 0;
+    for (int i = 0; i < N; ++i)
+    {
+        amax[count++] = apresum[i];
+        for (int j = i + 1; j < N; ++j)
+        {
+            amax[count++] = apresum[j] - apresum[i];
+        }
+    }
+    sort(amax, amax + count);
+    count = 0;
+    for (int i = 0; i < N; ++i)
+    {
+        bmax[count++] = bpresum[i];
+        for (int j = i + 1; j < N; ++j)
+        {
+            bmax[count++] = bpresum[j] - bpresum[i];
+        }
+    }
+    sort(bmax, bmax + count);
+    long long left = -10000000000000000LL, right = 10000000000000001LL, mid;
+    while (left < right)
+    {
+        mid = (right - left) / 2 + left;
+        if (CountGreaterProduct(amax, bmax, len, mid) >= K)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid;
+        }
+    }
+    return left;
 }
 
 int main()
 {
-	int ncase;
-	cin >> ncase;
-	for (int icase = 1; icase <= ncase; ++icase)
-	{
+    int ncase;
+    cin >> ncase;
+    for (int icase = 1; icase <= ncase; ++icase)
+    {
         cin >> N >> K >> A[0] >> B[0] >> c >> d >> e1 >> e2 >> f;
         x[0] = A[0];
         y[0] = B[0];
         r[0] = 0;
         s[0] = 0;
+        apresum[0] = A[0];
+        bpresum[0] = B[0];
         for (int i = 1; i < N; ++i)
         {
             x[i] = (c*x[i - 1] + d*y[i - 1] + e1) % f;
@@ -164,8 +249,19 @@ int main()
             s[i] = (d*r[i - 1] + c*s[i - 1] + e2) % 2;
             A[i] = (r[i] % 2 == 0 ? x[i] : -x[i]);
             B[i] = (s[i] % 2 == 0 ? y[i] : -y[i]);
+            apresum[i] = apresum[i - 1] + A[i];
+            bpresum[i] = bpresum[i - 1] + B[i];
         }
-        
-	}
-	return 0;
+        long long ans;
+        if(N < 200 || (long long)N * (N + 1) / 2 <= 2 * K)
+        {
+            ans = CalcSmall();
+        }
+        else
+        {
+            ans = CalcLarge();
+        }
+        cout << "Case #" << icase << ": " << ans << endl;
+    }
+    return 0;
 }
