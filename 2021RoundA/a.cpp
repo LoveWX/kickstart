a84e9c67d6f0fffa78577fbd441d19475356387a
LoveWX <ztf0025@gmail.com>
2021-08-16 00:15:54 +0800

    Update a.cpp

--- 2021RoundA/a.cpp
+++ 2021RoundA/a.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <map>
+#include <string>
 using namespace std;
 
 int main()
@@ -8,25 +8,18 @@ int main()
     cin>>ncase;
     for(int icase=1;icase<=ncase;++icase)
     {
-        int m[3][3];
-        cin>>m[0][0]>>m[0][1]>>m[0][2];
-        cin>>m[1][0]         >>m[1][2];
-        cin>>m[2][0]>>m[2][1]>>m[2][2];
-        map<int,int> mc;
-        if((m[1][2]-m[1][0])%2==0) mc[m[1][0]+(m[1][2]-m[1][0])/2]+=1;
-        if((m[2][1]-m[0][1])%2==0) mc[m[0][1]+(m[2][1]-m[0][1])/2]+=1;
-        if((m[2][2]-m[0][0])%2==0) mc[m[0][0]+(m[2][2]-m[0][0])/2]+=1;
-        if((m[2][0]-m[0][2])%2==0) mc[m[0][2]+(m[2][0]-m[0][2])/2]+=1;
-        int ans=0;
-        for(const auto &p:mc)
+        int N,K;
+        string S;
+        cin>>N>>K>>S;
+        int count=0;
+        for(int i=0,j=N-1;i<j;++i,--j)
         {
-            ans=max(ans,p.second);
+            if(S[i]!=S[j])
+            {
+                count+=1;
+            }
         }
-        if(m[2][0]-m[1][0]==m[1][0]-m[0][0]) ans+=1;
-        if(m[2][2]-m[1][2]==m[1][2]-m[0][2]) ans+=1;
-        if(m[0][2]-m[0][1]==m[0][1]-m[0][0]) ans+=1;
-        if(m[2][2]-m[2][1]==m[2][1]-m[2][0]) ans+=1;
-        cout<<"Case #"<<icase<<": "<<ans<<endl;
+        cout<<"Case #"<<icase<<": "<<abs(K-count)<<endl;
     }
     return 0;
 }
