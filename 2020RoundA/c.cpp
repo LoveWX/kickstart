8784dfd6d0a41a963b19e7ba7335352f6f76e89d
LoveWX <ztf0025@gmail.com>
2021-08-15 21:38:38 +0800

    Update c.cpp

--- 2020RoundA/c.cpp
+++ 2020RoundA/c.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
+#include <vector>
+#include <queue>
+#include <tuple>
 using namespace std;
 
-const int SIZE=1000;
-int mm[SIZE][SIZE];
-int le[SIZE][SIZE],ri[SIZE][SIZE],up[SIZE][SIZE],dn[SIZE][SIZE];
+const int SIZE=300;
+int grid[SIZE][SIZE];
 int R,C;
 
-inline int Calc(int c0,int c1) //c0:longer edge, c1:shorter edge
+inline bool InBoard(int x,int y)
 {
-    return (c1<2 || c0<4) ? 0 : min(c1-1,c0/2-1);
+    return 0<=x && x<R && 0<=y && y<C;
 }
 
 int main()
@@ -17,51 +19,29 @@ int main()
     cin>>ncase;
     for(int icase=1;icase<=ncase;++icase)
     {
-        int N,K;
         cin>>R>>C;
+        priority_queue<tuple<int,int,int>> pq;
         for(int i=0;i<R;++i)
         {
             for(int j=0;j<C;++j)
             {
-                cin>>mm[i][j];
+                cin>>grid[i][j];
+                pq.push({grid[i][j],i,j});
             }
         }
-        for(int i=0;i<R;++i)
+        long long ans=0LL;
+        while(!pq.empty())
         {
-            le[i][0]=mm[i][0];
-            for(int j=1;j<C;++j)
-            {
-                le[i][j]=(mm[i][j]==0 ? 0 : le[i][j-1]+1);
-            }
-            ri[i][C-1]=mm[i][C-1];
-            for(int j=C-2;j>=0;--j)
-            {
-                ri[i][j]=(mm[i][j]==0 ? 0 : ri[i][j+1]+1);
-            }
-        }
-        for(int j=0;j<C;++j)
-        {
-            up[0][j]=mm[0][j];
-            for(int i=1;i<R;++i)
-            {
-                up[i][j]=(mm[i][j]==0 ? 0 : up[i-1][j]+1);
-            }
-            dn[R-1][j]=mm[R-1][j];
-            for(int i=R-2;i>=0;--i)
-            {
-                dn[i][j]=(mm[i][j]==0 ? 0 : dn[i+1][j]+1);
-            }
-        }
-        int ans=0;
-        for(int i=0;i<R;++i)
-        {
-            for(int j=0;j<C;++j)
-            {
-                ans+=Calc(up[i][j],le[i][j])+Calc(up[i][j],ri[i][j]);
-                ans+=Calc(dn[i][j],le[i][j])+Calc(dn[i][j],ri[i][j]);
-                ans+=Calc(le[i][j],up[i][j])+Calc(le[i][j],dn[i][j]);
-                ans+=Calc(ri[i][j],up[i][j])+Calc(ri[i][j],dn[i][j]);
-            }
+            auto [hh,xx,yy]=pq.top();
+            pq.pop();
+            if(grid[xx][yy]<0) continue;
+            if(grid[xx][yy]<hh) ans+=hh-grid[xx][yy];
+            hh=max(grid[xx][yy],hh)-1;
+            grid[xx][yy]=-1;
+            if(InBoard(xx-1,yy) && grid[xx-1][yy]>=0) pq.push({hh,xx-1,yy});
+            if(InBoard(xx+1,yy) && grid[xx+1][yy]>=0) pq.push({hh,xx+1,yy});
+            if(InBoard(xx,yy-1) && grid[xx][yy-1]>=0) pq.push({hh,xx,yy-1});
+            if(InBoard(xx,yy+1) && grid[xx][yy+1]>=0) pq.push({hh,xx,yy+1});
         }
         cout<<"Case #"<<icase<<": "<<ans<<endl;
     }
