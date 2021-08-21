1315e12ac0430f1e4458028992f2a876667e05fe
LoveWX <ztf0025@gmail.com>
2021-08-22 00:20:46 +0800

    Update ShortestPath.cpp

--- snippet/ShortestPath.cpp
+++ snippet/ShortestPath.cpp
@@ -25,6 +25,39 @@ void ShortestPathDijkstra(vector<int> &vdis,int src)
     }
 }
 
+//如果需要记录最短路径有多少条，则使用map来模拟priority_queue使得相同的路径归并在一起计算
+vector<vector<vector<int>>> edge; // edge[i]={{j,dis(i,j)}, ...}
+void ShortestPathDijkstra(vector<long long> &vdis,vector<int> &vcnt,int src)
+{
+    map<pair<long long,int>,int> m;
+    m[{0,src}]=1;
+    while(!m.empty())
+    {
+        auto it=m.begin();
+        auto [dis,dst]=it->first;
+        int cnt=it->second;
+        m.erase(it);
+        if(dis>vdis[dst])
+        {
+            continue;
+        }
+        if(dis==vdis[dst])
+        {
+            vcnt[dst]=(vcnt[dst]+cnt)%mod;
+            continue;
+        }
+        vdis[dst]=dis;
+        vcnt[dst]=cnt;
+        for(vector<int> &vi:edge[dst])
+        {
+            if(dis+vi[1]<vdis[vi[0]])
+            {
+                m[{dis+vi[1],vi[0]}]=(m[{dis+vi[1],vi[0]}]+cnt)%mod;
+            }
+        }
+    }
+}
+
 //FloydWarshall算法计算全源最短路径，时间复杂度O(n^3)
 //使用邻接矩阵,将直连的边作为两点之间距离记录
 //尝试新增每一个点k,如果dis(i,k)+dis(k,j)<dis(i,j)，就将dis(i,j)更新为dis(i,k)+dis(k,j)
