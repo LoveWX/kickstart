4606c567674c9e6e5b1e0eb3f8a852e6d95a98ea
LoveWX <ztf0025@gmail.com>
2022-07-11 01:20:24 +0800

    Add files via upload

---
+++ snippet/threadpool.cpp
@@ -0,0 +1,96 @@
+#include <vector>
+#include <queue>
+#include <memory>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <future>
+#include <functional>
+#include <stdexcept>
+#include <iostream>
+// #include <vector>
+#include <chrono>
+
+struct Task
+{
+    Task(int i)
+    {
+        idx=i;
+    }
+    void operator()()
+    {
+        std::cout<<idx*idx<<std::endl;
+    }
+    int idx;
+};
+
+class ThreadPool
+{
+public:
+    ThreadPool(int size);
+    void AddTask(Task &newTask);
+    ~ThreadPool();
+private:
+    bool stop;
+    std::mutex mtx;
+    std::condition_variable cond;
+    std::queue<Task> taskQueue;
+    std::vector<std::thread> threads;
+};
+
+ThreadPool::ThreadPool(int size)
+{
+    stop=false;
+    auto func=[this](){
+        for(;;)
+        {
+            std::unique_lock<std::mutex> lock(mtx);
+            cond.wait(lock, [this](){
+                return stop || !taskQueue.empty();
+            });
+            if(stop && taskQueue.empty()) return;
+            Task task=std::move(taskQueue.front());
+            taskQueue.pop();
+            lock.unlock();
+            task();
+        }
+    };
+    for(int i=0;i<size;++i)
+    {
+        threads.emplace_back(std::thread(func));
+    }
+}
+
+void ThreadPool::AddTask(Task &newTask)
+{
+    if(stop) return;
+    std::unique_lock<std::mutex> lock(mtx);
+    taskQueue.emplace(newTask);
+    lock.unlock();
+    cond.notify_one();
+}
+
+ThreadPool::~ThreadPool()
+{
+    std::unique_lock<std::mutex> lock(mtx);
+    stop=true;
+    lock.unlock();
+    cond.notify_all();
+    for(auto &t:threads)
+    {
+        t.join();
+    }
+}
+
+int main()
+{
+    ThreadPool pool(4);
+    for(int i=0;i<8;++i)
+    {
+        std::cout << "hello " << i << std::endl;
+        pool.AddTask(Task(i));
+        std::cout << "world " << i << std::endl;
+    }
+
+    return 0;
+}
