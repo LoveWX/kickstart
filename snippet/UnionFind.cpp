c4360395b49d066b8827ff2c16fe71a4b94a8589
LoveWX <ztf0025@gmail.com>
2023-05-25 12:20:54 +0800

    Update UnionFind.cpp

--- snippet/UnionFind.cpp
+++ snippet/UnionFind.cpp
@@ -9,7 +9,6 @@
 //封装
 struct UnionFind
 {
-    int n;
     vector<int> parent;
     vector<int> rank;
     UnionFind(int n)
@@ -52,6 +51,54 @@ struct UnionFind
     }
 };
 
+//用集合数代替rank进行合并,可以求出某元素所在集合的大小
+struct UnionFind
+{
+    vector<int> parent;
+    vector<int> vcnt;
+    void Init(int n)
+    {
+        parent.assign(n,0);
+        for(int i=1;i<n;++i)
+        {
+            parent[i]=i;
+        }
+        vcnt.assign(n,1);
+    }
+    int findr(int x)
+    {
+        int r=x;
+        for(;r!=parent[r];r=parent[r]);
+        for(int i=x;r!=parent[i];)
+        {
+            int j=parent[i];
+            parent[i]=r;
+            i=j;
+        }
+        return r;
+    }
+    void unionr(int x,int y)//合并到大集合中,一样大时合并到x所在集合
+    {
+        int rx=findr(x),ry=findr(y);
+        if(rx==ry) return;
+        if(vcnt[rx]<vcnt[ry])
+        {
+            parent[rx]=ry;
+            vcnt[ry]+=vcnt[rx];
+        }
+        else
+        {
+            parent[ry]=rx;
+            vcnt[rx]+=vcnt[ry];
+        }
+    }
+    int count(int x)
+    {
+        return vcnt[findr(x)];
+    }
+};
+
+//散装
 int n;
 vector<int> parent;
 vector<int> rank;
