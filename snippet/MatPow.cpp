2c0c9750dcf40f6480c8966640a81514e79c00f7
LoveWX <ztf0025@gmail.com>
2026-06-24 11:54:21 +0800

    Update MatPow.cpp

--- snippet/MatPow.cpp
+++ snippet/MatPow.cpp
@@ -1,42 +1,55 @@
 const long long mod=1e9+7;
-vector<vector<long long>> MatMul(vector<vector<long long>> &a,vector<vector<long long>> &b)
+vector<long long> VecMulMat(vector<long long> &b,vector<vector<long long>> &c)
 {
-    size_t n=a.size();
-    vector<vector<long long>> c(n,vector<long long>(n,0));
-    for(size_t i=0U;i<n;++i){
-        for(size_t j=0U;j<n;++j){
-            long long &s=c[i][j];
-            for(size_t k=0U;k<n;++k){
-                s=(s+a[i][k]*b[k][j])%mod;
+    unsigned int n=b.size();
+    vector<long long> a(n,0);
+    for(unsigned int j=0U;j<n;++j){
+        long long sum=0;
+        for(unsigned int i=0U;i<n;++i){
+            sum+=b[i]*c[i][j]%mod;
+        }
+        a[j]=sum%mod;
+    }
+    return a;
+}
+vector<long long> MatMulVec(vector<vector<long long>> &b,vector<long long> &c)
+{
+    unsigned int n=b.size();
+    vector<long long> a(n,0);
+    for(unsigned int i=0U;i<n;++i){
+        long long sum=0;
+        for(unsigned int j=0U;j<n;++j){
+            sum+=b[i][j]*c[j]%mod;
+        }
+        a[i]=sum%mod;
+    }
+    return a;
+}
+vector<vector<long long>> MatMul(vector<vector<long long>> &b,vector<vector<long long>> &c)
+{
+    unsigned int n=b.size();
+    vector<vector<long long>> a(n,vector<long long>(n,0));
+    for(unsigned int i=0U;i<n;++i){
+        for(unsigned int j=0U;j<n;++j){
+            long long sum=0;
+            for(unsigned int k=0U;k<n;++k){
+                sum+=b[i][k]*c[k][j]%mod;
             }
+            a[i][j]=sum%mod;
         }
     }
-    return c;
+    return a;
 }
-
-vector<long long> VecMulMat(vector<long long> &a,vector<vector<long long>> &b)
+vector<vector<long long>> MatPow(vector<vector<long long>> m,int p)
 {
-    size_t n=a.size();
-    vector<long long> c(n,0);
-    for(size_t i=0U;i<n;++i){
-        long long &s=c[i];
-        for(size_t k=0U;k<n;++k){
-            s=(s+a[k]*b[k][i])%mod;
-        }
+    unsigned int n=m.size();
+    vector<vector<long long>> a(n,vector<long long>(n,0));
+    for(unsigned int i=0U;i<n;++i){
+        a[i][i]=1;
     }
-    return c;
-}
-
-vector<vector<long long>> MatPow(vector<vector<long long>> a,long long b)
-{
-    size_t n=a.size();
-    vector<vector<long long>> c(n,vector<long long>(n,0));
-    for(size_t i=0U;i<n;++i){
-        c[i][i]=1;
+    for(;p>0;p>>=1){
+        if(p&1) a=MatMul(a,m);
+        m=MatMul(m,m);
     }
-    for(;b;b>>=1){
-        if(b&1LL) c=MatMul(c,a);
-        a=MatMul(a,a);
-    }
-    return c;
+    return a;
 }
